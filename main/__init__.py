6ceb9e125dd961575ae15a87af65e459db45cd3f
Raul Rodrigo Segura <raurodse@gmail.com>
2020-06-30 15:07:14 +0200

    wip

--- main/__init__.py
+++ main/__init__.py
@@ -1,13 +1,44 @@
-from flask import Blueprint, render_template, abort, request
+from flask import Blueprint, render_template, abort, request, send_from_directory,url_for
 from jinja2 import TemplateNotFound
 from ainur.utils import validate_groups
 from flask_login import login_required
 
-exportmodule = Blueprint('mainmodule', __name__,template_folder='templates')
+from os.path import join as os_path_join, basename
+from json import load as json_load
+from glob import glob
+from codecs import open as codecs_open
+
+exportmodule = Blueprint('mainmodule', __name__,template_folder='templates',static_folder='static')
+
+BASE_PATH = '/usr/share/lliurex-www/srv/'
+LINKS_PATH = os_path_join(BASE_PATH,'links')
+OVERRIDE_PATH = '/var/lib/lliurex-www'
+OVERRIDE_LINKS_PATH = os_path_join(OVERRIDE_PATH,'links')
+ICONS_PATH = os_path_join(BASE_PATH,'icons')
 
 @validate_groups(['admins'])
 @login_required
 def main():
-    print(request)
-    return render_template('main/main.html')
+    resources = get_resources()
+    return render_template('main/main.html', resources=resources)
 
+def get_resources():
+    links = {}
+    for link in glob(os_path_join(LINKS_PATH,'*.json')):
+        with codecs_open(link,'r') as fd:
+            links[basename(link)] = json_load(fd)
+    
+    for link in glob(os_path_join(OVERRIDE_LINKS_PATH,'*.json')):
+        with codecs_open(link,'r') as fd:
+            basename_link = basename(link)
+            metadata = json_load(fd)
+            if basename_link in links:
+                links[basename_link] = {**links[basename_link], **metadata}
+            else:
+                links[basename_link] = metadata
+
+    return sorted(links.values(), key=lambda x: x['order'] if 'order' in x else 999)
+
+# @exportmodule.route('/static/<path:path>')
+# def send_js(path):
+#     return send_from_directory(ICONS_PATH, path)
